bff8066e2b126579bf87b8d0d7499badf7db9d74
Claudia2305 <claudia2305c@hotmail.com>
2025-03-18 10:41:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tirsdag-18.mars---19.gang---Mer-Arrays-
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Claudia2305/Tirsdag-18.mars---19.gang---Mer-Arrays-)
---
+++ script.js
@@ -0,0 +1,65 @@
+// Flat array
+const movies = [
+  'Inception',
+  'The Matrix',
+  'Interstellar',
+  'The Dark Knight',
+  'Pulp Fiction',
+];
+
+console.log(movies);
+console.log(movies[2]);
+
+//
+//
+
+// Object with arrays
+const groceries = {
+  fruits: ['apples', 'bananas', 'oranges'],
+  vegetables: ['carrots', 'broccoli', 'spinach'],
+  dairy: ['milk', 'cheese', 'yogurt'],
+};
+
+console.log(groceries);
+console.log(groceries.fruits[0]);
+console.log(groceries.vegetables[2]);
+console.log(groceries.dairy[1]);
+
+//
+//
+
+// Array with objects
+const onlineStore = [
+  {
+    name: 'Laptop',
+    price: 1200,
+    inStock: true,
+  },
+  {
+    name: 'Phone',
+    price: 800,
+    inStock: false,
+  },
+  {
+    name: 'Headphones',
+    price: 150,
+    inStock: true,
+  },
+];
+
+console.log(onlineStore);
+console.log(onlineStore[0].name);
+console.log(onlineStore[2].price);
+console.log(onlineStore[0].inStock);
+
+//
+//
+//
+
+const handlesliste = [];
+
+const pushButton = document.querySelector('#pushButton');
+
+pushButton.addEventListener('click', function () {
+  console.log('Knapp er trykket!');
+});
